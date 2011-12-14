d4ae47d1b73e4c58b5b03ec9ae44dd823742797a
Othmar Gsenger <otti@realraum.at>
2011-12-14 14:58:59 +0000

    improved plots

--- pc/plot.py
+++ pc/plot.py
@@ -5,7 +5,7 @@ import numpy
 import pylab
 for datastr in fileinput.input():
   data=numpy.array(list(datastr))
-  pylab.plot(range(len(data)),data)
-
+  pylab.plot(range(len(datastr)),data)
+  pylab.axis([0, 2000,-0.5, 1.5])
           
 pylab.show()
